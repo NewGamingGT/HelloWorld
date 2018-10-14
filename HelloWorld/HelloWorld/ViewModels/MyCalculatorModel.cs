a1eeeeda640e129cb0d43792acef35f6d0ba76a3
NewGamingGT <39025657+NewGamingGT@users.noreply.github.com>
2018-10-14 16:01:34 +0800

    Initial commit

---
+++ HelloWorld/HelloWorld/ViewModels/MyCalculatorModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using HelloWorld.Annotations;
+using Xamarin.Forms;
+
+namespace HelloWorld.ViewModels
+{
+    public class MyCalculatorModel: INotifyPropertyChanged
+    {
+        private string _result = "888";
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand ClearCommand=>new Command(() => { Result = string.Empty; });
+        public string Title { get; set; } = "My Calculator";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Views/MyCalculator.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MyCalculatorPage : ContentPage
+	{
+		public MyCalculatorPage()
+		{
+			InitializeComponent ();
+		    PlusMinusSign.Text = ((char)0x00B1).ToString();
+		    BackSign.Text = ((char)0x2B05).ToString();
+		    DivideSign.Text = ((char) 0xF7).ToString();
+		    Multiplysign.Text = ((char)0x2A09).ToString();
+            MinusSign.Text = ((char)0x2212).ToString();
+        }
+    }
+}
